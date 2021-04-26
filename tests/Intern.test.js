1bb2d853d8cfad98be7d0ef1e0482aecd11c3452
Jada Desormeaux <JadaE789@gmail.com>
2021-04-25 21:53:39 -0400

    tests changes

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const testValue = "BSU";
+  const e = new Intern("Kaila", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Tera", 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const testValue = "TU";
+  const e = new Intern("Tiff", 1, "test@test.com", testValue);
   expect(e.getSchool()).toBe(testValue);
 });
